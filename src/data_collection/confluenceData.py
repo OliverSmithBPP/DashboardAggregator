dd9f40dd3ff06f593678920dd89e2b5162ad267f
OliverSmithBPP <o.smith11@my.bpp.com>
2026-04-16 18:27:12 +0100

    getting ticket data from zoho

--- src/data_collection/confluenceData.py
+++ src/data_collection/confluenceData.py
@@ -1,12 +1,11 @@
-from atlassian import Confluence
 import os
+from atlassian import Confluence
 
 
 CONFLUENCE_EMAIL = os.environ.get("JIRA_EMAIL")
 CONFLUENCE_API_TOKEN = os.environ.get("JIRA_API_TOKEN")
 
 CONFLUENCE_URL = 'https://projectdashboardexample.atlassian.net/wiki'
-
 
 confluence = Confluence(
     url=CONFLUENCE_URL,
@@ -15,10 +14,7 @@ confluence = Confluence(
 )
 
 
-def collect_confluence_data(pageId):
+
+def get_confluence_data(pageId):
     page = confluence.get_page_by_id(pageId, expand='body.storage')
     print(f"Page = {page['body']['storage']['value']}")
-
-
-if __name__ == "__main__":
-    collect_confluence_data('229540')
--- src/data_collection/jiraData.py
+++ src/data_collection/jiraData.py
@@ -11,6 +11,6 @@ jira = JIRA(options=jiraOptions, basic_auth=(JIRA_EMAIL, JIRA_API_TOKEN))
 
 
 
-def collect_jira_data(project):
+def get_jira_data(project):
     for issue in jira.search_issues(jql_str=f'project = {project}'):
         print(f'Key = {issue.key}, Summary = {issue.fields.summary}, Name = {issue.fields.reporter.displayName}')
