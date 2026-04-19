c507ce7735f45593762a41ffe36168820e17e019
OliverSmithBPP <o.smith11@my.bpp.com>
2026-04-19 20:58:15 +0100

    creating json file from jira data

--- src/data_collection/jiraData.py
+++ src/data_collection/jiraData.py
@@ -1,4 +1,5 @@
 import os
+import json
 from jira import JIRA
 
 JIRA_EMAIL = os.environ.get("JIRA_EMAIL")
@@ -11,6 +12,32 @@ jira = JIRA(options=jiraOptions, basic_auth=(JIRA_EMAIL, JIRA_API_TOKEN))
 
 
 
-def get_jira_data(project):
-    for issue in jira.search_issues(jql_str=f'project = {project}'):
-        print(f'Key = {issue.key}, Summary = {issue.fields.summary}, Name = {issue.fields.reporter.displayName}')
+def get_jira_data_for_project(project):
+    issues = jira.search_issues(jql_str=f'project = {project}')
+    issue_list = []
+    for issue in issues:
+         issue_list.append(convert_issue_to_dict(issue))
+    if len(issue_list) != 0:
+        write_jira_data_to_json(issue_list) 
+    else:
+        print("No JIRA issues found, not writing to JSON file")        
+
+
+def convert_issue_to_dict(issue):
+    issue_dict = {
+        "id": issue.id,
+        "key": issue.key,
+        "summary": issue.fields.summary,
+        "status": issue.fields.status.name,
+        "assignee": issue.fields.assignee.displayName if issue.fields.assignee else None,
+        "reporter": issue.fields.reporter.displayName if issue.fields.reporter else None,
+        "priority": issue.fields.priority.name if issue.fields.priority else None,
+        "issue_type": issue.fields.issuetype.name,
+        "created": issue.fields.created,
+    }
+    return issue_dict 
+
+
+def write_jira_data_to_json(issue_list):
+    with open("jira_issues_json", "w", encoding="utf-8") as file:
+        json.dump(issue_list, file, indent=2, ensure_ascii=False)
